7be8dd7089541b9a424b1750b5c60429ac5d4e50
FX Foo <78995824+foofx88@users.noreply.github.com>
2021-08-23 11:45:48 +0800

    removed dots as it is redundant

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,8 +19,8 @@ function animateBlock() {
             return anime.random(1, 6);
         },
         easing: 'linear',
-        duration: 8000,
-        delay: anime.stagger(30),
+        duration: 10000,
+        delay: anime.stagger(60),
         complete: animateBlock,
     }) 
 }
@@ -50,9 +50,6 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
 }
