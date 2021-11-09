ee28f92cd54a92d482cf505a8c723e869eb3a13c
FX Foo <fangxuan.foo@gmail.com>
2021-11-09 10:56:54 +0800

    Updated Functions with Navigation Keys

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,7 +58,6 @@ function handleKeyboardNav(e) {
         if (!e) e = window.event;
         var kc = e.keyCode;
         if (kc == 37) plusSlides(-1);
-            console.log("wow!")
         if (kc == 39) plusSlides(1);
     }
 
